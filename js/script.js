cfa8e5f7d9e637555c7cb6b9fe05f4ef7cf73259
Resi Respati <resir014@gmail.com>
2014-04-17 17:07:27 +0700

    Insert content.

--- js/script.js
+++ js/script.js
@@ -13,15 +13,6 @@ $(function () {
     randomSplash[7]     = "At what time do you think Thorin wakes up in the morning in order to braid his hair?";
     randomSplash[8]     = "*whispers* Hail HYDRA!";
     randomSplash[9]     = "Looking for hiatus buddy? We’re professionals.";
-    randomSplash[10]    = "Are we still waiting for Sherlock? How long do we have to wait?";
-    randomSplash[11]    = "Sherlock, for the last time, you're not bloody Godot.";
-    randomSplash[12]    = "Do you think the Doctor would like his kidneys by the time of the premiere? <br /> Find out with us.";
-    randomSplash[13]    = "Do you split your sides laughing like Beverly Katz did?";
-    randomSplash[14]    = "Nothing here is vegetarian. <br /> We are all humanitarian.";
-    randomSplash[15]    = "By the way, did we also mention we travel in space and time?";
-    randomSplash[16]    = "GOOD LORD, SIMON.";
-    randomSplash[17]    = "Now isn't the winter of our discontent (yet).";
-    randomSplash[18]    = "And now, the weather.";
 
     var generateRandomNumber = Math.floor(Math.random()*randomSplash.length);
 
